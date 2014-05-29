28b642f5a3907ce97afe72006f3d6bddf76d1bf8
Matthew Duguay <matthew.duguay@gmail.com>
2014-05-28 21:09:51 -0400

    Created User controller and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 	
 	def full_title(page_title)
-		base_title = "RoR Tutorial Sample App"
+		base_title = "Twitter Clone Tutorial"
 		if page_title.empty?
 			base_title
 		else
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,24 +7,24 @@ describe "StaticPages" do
   describe "Contact Page" do
     before { visit contact_path }
     it { should have_content('Contact') }
-    it { should have_title("RoR Tutorial Sample App | Contact") }
+    it { should have_title(full_title("Contact")) }
   end
 
   describe "Home Page" do
     before { visit root_path }
     it { should have_content('Sample App') }
-    it { should have_title("RoR Tutorial Sample App | Home") }
+    it { should have_title(full_title("Home")) }
   end
 
   describe "Help Page" do
     before { visit help_path }
   	it { should have_content('Help') }
-    it { should have_title("RoR Tutorial Sample App | Help") }
+    it { should have_title(full_title("Help")) }
   end
 
   describe "About Page" do
     before { visit about_path }
     it { should have_content('About Us') }
-    it { should have_title("RoR Tutorial Sample App | About") }
+    it { should have_title(full_title("About")) }
   end
 end
