477ede68e2c5d28896222167fc403cbd48e21a13
Matthew Duguay <matthew.duguay@gmail.com>
2014-05-28 20:11:09 -0400

    Refactor links with Rails Routing

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,39 +1,30 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  describe "Home Page" do
-    it "should contain the words 'Sample App' somewhere" do
-    	visit '/static_pages/home'
-    	expect(page).to have_content('Sample App')
-    end
 
-    it "should have the right title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("RoR Tutorial Sample App | Home")
-    end
+  subject { page }
+
+  describe "Contact Page" do
+    before { visit contact_path }
+    it { should have_content('Contact') }
+    it { should have_title("RoR Tutorial Sample App | Contact") }
+  end
+
+  describe "Home Page" do
+    before { visit root_path }
+    it { should have_content('Sample App') }
+    it { should have_title("RoR Tutorial Sample App | Home") }
   end
 
   describe "Help Page" do
-  	it "should contain the word 'Help'" do
-  		visit '/static_pages/help'
-  		expect(page).to have_content('Help')
-  	end
-
-    it "should have the right title" do
-      visit '/static_pages/help'
-      expect(page).to have_title("RoR Tutorial Sample App | Help")
-    end
+    before { visit help_path }
+  	it { should have_content('Help') }
+    it { should have_title("RoR Tutorial Sample App | Help") }
   end
 
   describe "About Page" do
-    it "should contain the phrase 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About Us')
-    end
-
-    it "should have the right title" do
-      visit '/static_pages/about'
-      expect(page).to have_title("RoR Tutorial Sample App | About")
-    end
+    before { visit about_path }
+    it { should have_content('About Us') }
+    it { should have_title("RoR Tutorial Sample App | About") }
   end
 end
