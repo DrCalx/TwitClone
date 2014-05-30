6af69a59a4f102ed23a17aaeaf1ef16ae5e00617
Matthew Duguay <matthew.duguay@gmail.com>
2014-05-30 14:37:13 -0400

    Add User index on email. Email Validation and uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
-	validates :name, 	presence: true, length: { maximum: 50 }
-	validates :email, 	presence: true
+	#Constant
+	EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+	#Validations
+	validates :name, 	presence: true, length: { maximum: 50 }	
+	validates :email, 	presence: true, format: { with: EMAIL_REGEX },
+						uniqueness: { case_sensitive: false }
+
+	#Callbacks
+	before_save { self.email = self.email.downcase }
 end
