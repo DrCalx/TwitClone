eb88ce61c071b769cbf8a4c4f0eda910453d8a86
Matthew Duguay <matthew.duguay@gmail.com>
2014-05-30 13:54:04 -0400

    Added User validation and RSpec tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @user = User.new(name: "Example User", email: "user@example.com") }
+
+  subject { @user }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+
+  it { should be_valid }
+
+  describe "should not allow blank names" do
+  	before { @user.name = " " }
+  	it { should_not be_valid }
+  end
+
+  describe "should not allow blank emails" do
+  	before { @user.email = " " }
+  	it { should_not be_valid }
+  end
+
+  describe "should not allow long names" do
+  	before { @user.name = 'a' * 51 }
+  	it { should_not be_valid }
+  end
 end
