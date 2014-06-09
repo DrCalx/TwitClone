032802b4e0d85e7fc88a405748914d1fa9b0d19e
Matthew Duguay <matthew.duguay@gmail.com>
2014-06-09 11:47:53 -0400

    Create User signup RSpec tests

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -14,4 +14,28 @@ describe "UserPages" do
 		it { should have_content(user.name) }
 		it { should have_title(user.name) }
 	end
+
+	describe "signup" do
+		before { visit signup_path }
+		let(:submit) { "Create my account" }
+
+		describe "with blank information" do
+			it "should not create a user" do
+				expect { click_button submit }.not_to change(User, :count)
+			end
+		end
+
+		describe "with valid information" do
+			before do
+				fill_in "Name", 	with: "Test User"
+				fill_in "Email", 	with: "test@test.com"
+				fill_in "Password", 	with: "Pass123"
+				fill_in "Confirmation", with: "Pass123"
+			end
+
+			it "should create user" do
+				expect { click_button submit }.to change(User, :count).by(1)
+			end
+		end
+	end
 end
