ad392a1b1b52311dcbb17d1279008cbbbad346bd
Matthew Duguay <matthew.duguay@gmail.com>
2014-06-10 15:41:41 -0400

    Add flash result to user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
+			flash[:success] = "Congrats on signing up!"
 			redirect_to @user #why the hell does this work?
 		else
 			render 'new' #add flash with fail tokens? Don't need to. Rails puts erros in User object.
