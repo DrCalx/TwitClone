f6f72a4c88d451b4f2d68059dc2b46eab464f301
Matthew Duguay <matthew.duguay@gmail.com>
2014-06-11 16:40:30 -0400

    User signin DB lookup and routing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,16 @@ class SessionsController < ApplicationController
 	def new
 	end
 
+	def create
+		user = User.find_by(email: params[:session][:email].downcase) #params come from form on signin page
+		if user && user.authenticate(params[:session][:password])
+			redirect_to user
+		else
+			flash.now[:error] = 'Invalid email/password'
+			render 'new'
+		end
+	end
+
 	def destroy
 	end
 end
