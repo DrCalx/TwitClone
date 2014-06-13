5ec322e078b92fbdc244f49d0232252b052d706d
Matthew Duguay <matthew.duguay@gmail.com>
2014-06-13 19:06:29 -0400

    Only authorized users may edit profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+	before_action :signed_in_user, 	only: [:edit, :update, :delete]
+	before_action :correct_user,	only: [:edit, :update]
+
 	def show
 		@user = User.find(params[:id])
 	end
@@ -19,11 +22,9 @@ class UsersController < ApplicationController
 	end
 
 	def edit
-		@user = User.find(params[:id])
 	end
 
 	def update
-		@user = User.find(params[:id])
 		if @user.update_attributes(user_params)
 			flash[:success] = "Profile Updated"
 			redirect_to @user
@@ -33,7 +34,19 @@ class UsersController < ApplicationController
 	end
 
 	private 
-	def user_params
-		params.require(:user).permit(:name, :email, :password, :password_confirmation)
-	end
+		def user_params
+			params.require(:user).permit(:name, :email, :password, :password_confirmation)
+		end
+
+		def signed_in_user
+			unless signed_in?
+				flash[:notice] = "Please sign in"
+				redirect_to signin_url
+			end
+		end
+
+		def correct_user
+			@user = User.find(params[:id])
+			redirect_to(root_url) unless current_user?(@user)
+		end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "AuthenticationPages" do
+describe "Authentication" do
 	subject { page }
 
 	describe "signin page" do
@@ -32,6 +32,41 @@ describe "AuthenticationPages" do
 			it { should have_link('Settings', 		href:edit_user_path(factoryUser)) }
 			it { should have_link('Sign out', 		href:signout_path) }
 			it { should_not have_link('Sign in', 	href:signin_path) }
+		end
+	end
+
+	describe "authorization" do
+		describe "for users not signed in" do
+			let(:user) { FactoryGirl.create(:user) }
+
+			describe "in Users controller" do
+				describe "visiting the edit page" do
+					before { visit edit_user_path(user) }
+					it { should have_title('Sign in') }
+				end
+
+				describe "submitting an update" do
+					before { patch user_path(user) }
+					specify { expect(response).to redirect_to(signin_path) }
+				end
+			end
+		end
+
+		describe "as wrong user" do
+			let(:user) { FactoryGirl.create(:user) }
+			let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@user.com") }
+			before{ valid_signin user }
+
+			describe "submit a GET to Users#edit action" do
+				before { get edit_user_path(wrong_user) }
+				specify { expect(response.body).not_to match(full_title('Edit user')) }
+				specify { expect(response).to redirect_to(root_url) }
+			end
+
+			describe "submit a PATCH to Users#update action" do
+				before { patch user_path(wrong_user) }
+				specify { expect(response).to redirect_to(root_url) }
+			end
 		end
 	end
 end
