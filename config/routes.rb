ceff14de763ef813568c7b983a5f61964e8abbf5
Matthew Duguay <matthew.duguay@gmail.com>
2014-06-22 16:06:56 -0400

    Add follow / unfollow buttons to foreign user pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,11 @@ SampleApp::Application.routes.draw do
       get :followed, :followers
     end
   end
-  resources :sessions, only: [:new, :create, :destroy]
-  resources :microposts, only: [:create, :destroy]
+  
+  resources :sessions,      only: [:new, :create, :destroy]
+  resources :microposts,    only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
+
   root "static_pages#home"
 
   match '/help',    to: 'static_pages#help',    via: 'get'
