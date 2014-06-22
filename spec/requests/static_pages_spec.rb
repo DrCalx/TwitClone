ea1e26f98a5508ad2c62adaf69c55a97bdfda0da
Matthew Duguay <matthew.duguay@gmail.com>
2014-06-22 15:45:01 -0400

    Add following / followed counters to home page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -37,6 +37,16 @@ describe "StaticPages" do
           expect(page).to have_selector("li##{item.id}", text: item.content)
         end
       end
+
+      describe "show followed / follower counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("1 follower", href: follower_user_path(user)) }
+        it { should have_link("0 followed", href: followed_user_path(user)) }
     end
   end
 
